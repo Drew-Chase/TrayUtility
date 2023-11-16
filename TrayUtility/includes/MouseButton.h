3003f2e4179646e15ce0ff6abb505ac0293f5da5
Drew Chase <dcmanproductions@gmail.com>
2023-11-16 00:02:04 -0500

    added mouse button enumeration

---
+++ TrayUtility/includes/MouseButton.h
@@ -0,0 +1,11 @@
+// LFInteractive LLC. (c) 2020-2024 All Rights Reserved
+#pragma once
+enum MouseButton
+{
+	Left,
+	Right,
+	Middle,
+	Left_Double_Click,
+	Right_Double_Click,
+	Middle_Double_Click,
+};
