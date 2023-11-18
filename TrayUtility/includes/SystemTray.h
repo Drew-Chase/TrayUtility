ef61f3e12178abcb4ace3e0f6adfdda7ed183fd6
Drew Chase <dcmanproductions@gmail.com>
2023-11-17 23:55:32 -0500

    added documentation, add/remove menu item, set icon, set tooltip, open context menu, and modified create function

--- TrayUtility/includes/SystemTray.h
+++ TrayUtility/includes/SystemTray.h
@@ -1,15 +1,86 @@
 // LFInteractive LLC. (c) 2020-2024 All Rights Reserved
 #pragma once
 #include <Windows.h>
+#include <vector>
+#include <string>
+#include "MenuItem.h"
 #include "MouseButton.h"
+
+using std::vector;
+using std::string;
+
 class SystemTray
 {
 private:
 	using IconInteractionCallback = void (*)(MouseButton button);
 	HWND hwnd;
 	NOTIFYICONDATA nid;
+	HICON hIcon;
+	IconInteractionCallback on_click_callback;
+	vector<MenuItem*> menu_items;
+	void UpdateShellIcon();
 public:
 	SystemTray();
 	~SystemTray();
-	bool Create(const wchar_t* label, const wchar_t* unique_class_name, IconInteractionCallback callback);
+	/// <summary>
+	/// Opens the context menu for the tray icon.
+	/// </summary>
+	void OpenContextMenu();
+	/// <summary>
+	/// Creates a tray icon.
+	/// </summary>
+	/// <param name="label">The label of the tray icon</param>
+	/// <param name="unique_class_name">The unique class name of the tray icons window</param>
+	/// <param name="on_click">The on click callback event</param>
+	/// <returns></returns>
+	bool Create(const wchar_t* label, const wchar_t* unique_class_name, IconInteractionCallback on_click);
+	/// <summary>
+	/// Adds a menu item to the tray icon.
+	/// </summary>
+	/// <param name="label">The menu items text</param>
+	/// <param name="on_click">The on click callback event</param>
+	void AddMenuItem(const wchar_t* label, MenuItemClickCallback on_click);
+	/// <summary>
+	/// Adds a menu item to the tray icon.
+	/// </summary>
+	/// <param name="item"></param>
+	void AddMenuItem(MenuItem* item);
+	/// <summary>
+	/// Removes a menu item from the tray icon.
+	/// </summary>
+	/// <param name="item"></param>
+	void RemoveMenuItem(MenuItem* item);
+	/// <summary>
+	/// Removes a menu item from the tray icon.
+	/// </summary>
+	/// <param name="index"></param>
+	void RemoveMenuItem(unsigned int index);
+	/// <summary>
+	/// Sets the icon of the tray icon.
+	/// </summary>
+	/// <param name="icon_path"></param>
+	/// <returns></returns>
+	bool SetIcon(const wchar_t* icon_path);
+	/// <summary>
+	/// Sets the icon of the tray icon.
+	/// </summary>
+	/// <param name="icon"></param>
+	/// <returns></returns>
+	bool SetIcon(HICON icon);
+	/// <summary>
+	/// Sets the icon of the tray icon.
+	/// </summary>
+	/// <param name="icon_resource"></param>
+	/// <returns></returns>
+	bool SetIcon(LPWSTR icon_resource);
+	/// <summary>
+	/// Sets the tooltip of the tray icon.
+	/// </summary>
+	/// <param name="tooltip"></param>
+	void SetTooltip(const wchar_t* tooltip);
+	/// <summary>
+	/// Sets the tooltip of the tray icon.
+	/// </summary>
+	/// <param name="tooltip"></param>
+	void SetTooltip(wchar_t* tooltip);
 };
--- TrayUtility/src/SystemTray.cpp
+++ TrayUtility/src/SystemTray.cpp
@@ -1,10 +1,240 @@
 // LFInteractive LLC. (c) 2020-2024 All Rights Reserved
 #include "SystemTray.h"
+#include <string>
+
+void SystemTray::UpdateShellIcon()
+{
+	Shell_NotifyIconW(NIM_MODIFY, &nid);
+}
 
 SystemTray::SystemTray()
 {
+	this->nid = { 0 };
+	this->hIcon = nullptr;
+	this->hwnd = nullptr;
+	this->on_click_callback = nullptr;
 }
 
 SystemTray::~SystemTray()
 {
+	if (!this->menu_items.empty())
+	{
+		this->menu_items.clear();
+	}
+}
+bool SystemTray::Create(const wchar_t* label, const wchar_t* unique_class_name, IconInteractionCallback on_click)
+{
+	this->on_click_callback = on_click;
+
+	// Register the window class.
+	HINSTANCE hInstance = GetModuleHandle(NULL); // Gets the module instance
+
+	// Register the window class.
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.lpszClassName = unique_class_name;
+	wc.hInstance = hInstance;
+
+	// Set the function pointer as user data during the creation of the window
+	wc.cbWndExtra = sizeof(LONG_PTR);
+	wc.lpfnWndProc = [](HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) -> LRESULT
+	{
+		auto tray = reinterpret_cast<SystemTray*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+		switch (msg)
+		{
+			case WM_CREATE:
+			{
+				break;
+			}
+			case WM_USER + 0x6897:
+			{
+				tray->hwnd = hwnd;
+				switch (lParam)
+				{
+					case WM_LBUTTONUP:
+					{
+						tray->on_click_callback(MouseButton::Left);
+						break;
+					}
+					case WM_RBUTTONUP:
+					{
+						tray->on_click_callback(MouseButton::Right);
+						break;
+					}
+				}
+				break;
+			}
+			case WM_COMMAND:
+			{
+				auto commandID = LOWORD(wParam);
+				MouseButton button = MouseButton::Left;
+
+				switch (lParam)
+				{
+					case WM_LBUTTONUP:
+					{
+						button = MouseButton::Left;
+						break;
+					}
+					case WM_RBUTTONUP:
+					{
+						button = MouseButton::Right;
+						break;
+					}
+				}
+
+				for (auto item : tray->menu_items)
+				{
+					if (item->comandID == commandID)
+					{
+						item->on_click_callback(button);
+					}
+				}
+
+				break;
+			}
+			case WM_DESTROY:
+				PostQuitMessage(0);
+				break;
+			default:
+				return DefWindowProc(hwnd, msg, wParam, lParam);
+		}
+		return 0;
+	};
+
+	if (!RegisterClassExW(&wc))
+	{
+		MessageBoxW(NULL, (L"Unable to register the window class. Error code: " + std::to_wstring(GetLastError())).c_str(), L"Error", MB_OK | MB_ICONERROR);
+		return false;
+	}
+
+	// Create the window and parent it to the tray window.
+	HWND explorerHandle = FindWindowW(L"Shell_TrayWnd", NULL);
+	HWND window = CreateWindowExW(0, unique_class_name, L"", 0, 0, 0, 0, 0, explorerHandle, NULL, hInstance, this);
+
+	// Set the function pointer as user data during the creation of the window
+	SetWindowLongPtr(window, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
+
+	// Create the tray icon.
+	this->nid.cbSize = sizeof(NOTIFYICONDATA);
+	this->nid.hWnd = window;
+	this->nid.uID = 0;
+	this->nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP | NIF_SHOWTIP;
+	this->nid.uCallbackMessage = WM_USER + 0x6897;
+	HICON icon = LoadIconW(hInstance, IDI_APPLICATION);
+	if (icon)
+	{
+		this->nid.hIcon = icon;
+	}
+	wcscpy_s(this->nid.szTip, label);
+
+	if (!Shell_NotifyIconW(NIM_ADD, &nid))
+	{
+		MessageBoxW(NULL, (L"Unable to create the system tray icon. Error code: " + std::to_wstring(GetLastError())).c_str(), L"Error", MB_OK | MB_ICONERROR);
+		return false;
+	}
+
+	return true;
+}
+
+void SystemTray::AddMenuItem(MenuItem* item)
+{
+	item->comandID = this->menu_items.size() + 1;
+	this->menu_items.push_back(item);
+}
+
+void SystemTray::AddMenuItem(const wchar_t* label, MenuItemClickCallback on_click_callback)
+{
+	auto item = new MenuItem();
+	item->label = label;
+	item->comandID = this->menu_items.size() + 1;
+	item->on_click_callback = on_click_callback;
+	this->AddMenuItem(item);
+}
+
+void SystemTray::RemoveMenuItem(MenuItem* item)
+{
+	int index = 0;
+	for (auto i : this->menu_items)
+	{
+		if (i == item)
+		{
+			this->menu_items.erase(this->menu_items.begin() + index);
+			delete(item);
+			break;
+		}
+		index++;
+	}
+}
+
+void SystemTray::RemoveMenuItem(unsigned int index)
+{
+	MenuItem* item = this->menu_items.at(index);
+	this->menu_items.erase(this->menu_items.begin() + index);
+	delete(item);
+}
+
+bool SystemTray::SetIcon(HICON icon)
+{
+	// Release the existing icon if it was loaded
+	if (hIcon)
+	{
+		DestroyIcon(hIcon);
+	}
+
+	// Set the new icon
+	hIcon = icon;
+	nid.hIcon = icon;
+
+	// Update the tray icon
+	this->UpdateShellIcon();
+
+	return icon != nullptr;
+}
+
+bool SystemTray::SetIcon(const wchar_t* icon_path)
+{
+	// Load the icon from the file
+	HICON icon = (HICON)LoadImageW(nullptr, icon_path, IMAGE_ICON, 0, 0, LR_LOADFROMFILE | LR_DEFAULTSIZE | LR_SHARED);
+
+	if (!icon)
+	{
+		// Handle the error, for example, show a message box
+		MessageBoxW(NULL, L"Failed to load the icon from file", L"Error", MB_OK | MB_ICONERROR);
+		return false;
+	}
+
+	// Set the icon
+	return SetIcon(icon);
+}
+
+bool SystemTray::SetIcon(LPWSTR icon_resource)
+{
+	HICON icon = LoadIconW(GetModuleHandle(NULL), icon_resource);
+	return SetIcon(icon);
+}
+
+void SystemTray::SetTooltip(const wchar_t* tooltip)
+{
+	wcscpy_s(this->nid.szTip, const_cast<wchar_t*>(tooltip));
+	this->UpdateShellIcon();
+}
+
+void SystemTray::SetTooltip(wchar_t* tooltip)
+{
+	wcscpy_s(this->nid.szTip, const_cast<wchar_t*>(tooltip));
+	this->UpdateShellIcon();
+}
+
+void SystemTray::OpenContextMenu()
+{
+	POINT pt;
+	GetCursorPos(&pt);
+
+	HMENU menu = CreatePopupMenu();
+	for (auto item : this->menu_items)
+	{
+		AppendMenuW(menu, MF_STRING, item->comandID, item->label);
+	}
+	TrackPopupMenu(menu, TPM_LEFTALIGN | TPM_RIGHTBUTTON, pt.x, pt.y, 0, this->hwnd, NULL);
 }
