d75ac4953b442cb5cd9d6e8d09768f75c77b433b
Drew Chase <dcmanproductions@gmail.com>
2023-11-17 23:54:22 -0500

    added menu item struct

---
+++ TrayUtility/includes/MenuItem.h
@@ -0,0 +1,10 @@
+// LFInteractive LLC. (c) 2020-2024 All Rights Reserved
+#pragma once
+#include "MouseButton.h"
+using MenuItemClickCallback = void (*)(MouseButton button);
+struct MenuItem
+{
+	const wchar_t* label;
+	unsigned int comandID;
+	MenuItemClickCallback on_click_callback;
+};
